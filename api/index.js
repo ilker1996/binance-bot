062928896aa5172c8f2afb4535ea81ea2c13504a
ilker <19080242+ilker1996@users.noreply.github.com>
2022-09-17 18:45:16 +0300

    Remove unnecessary files

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const config = require("../config.json");
 
 platforms = {
   "Binance" : require('./binance_api'),
-  "Gate.io" : require('./gateio_api'),
 };
 
 module.exports = platforms[config.platform];
